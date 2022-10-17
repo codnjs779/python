fc25e96835dee80ae6ae6b648ee1ffebd309d96b
최채원 <codnjs779@naver.com>
2022-10-17 21:46:03 +0900

    파이썬 기초문법

---
+++ hello.py
@@ -0,0 +1,30 @@
+print("hello")
+# 위에 내용 참고 부탁드립니다~ 
+a = 3
+b = 7
+
+print(a+b)
+
+c = 'lifidifdifdifdfidfi \n dfjdkfjdkfdjfkd'
+print(c)
+
+d = """heldof 
+dkfjdkf
+kdfdkf
+"""
+
+string = 'I ate %d apples' %3
+
+name = 'chh'
+old = 2
+paaa = 'hello my name is %s. how old are you? %d' %(name, old)
+print(paaa)
+print(string)
+print(c+d)
+print((c+d)*2)
+
+dssdf = 'dfd{name}'.format(name="this")
+print(dssdf)
+
+dfd = f'hee{name}'
+print(dfd)
